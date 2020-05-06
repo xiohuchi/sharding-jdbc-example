6c0127047e9012d6ada43293a550ddab2d20dfaa
yangbin <1250211588@qq.com>
2020-05-06 23:09:25 +0800

    主从mysql

--- sharding-jdbc-master-slave/src/main/java/com/shardingjdbc/service/UserService.java
+++ sharding-jdbc-master-slave/src/main/java/com/shardingjdbc/service/UserService.java
@@ -4,6 +4,9 @@ import com.shardingjdbc.entity.User;
 
 import java.util.List;
 
+/**
+ * @author yangbin
+ */
 public interface UserService {
 
     Long addUser(User user);
--- sharding-jdbc-master-slave/src/main/java/com/shardingjdbc/service/impl/UserServiceImpl.java
+++ sharding-jdbc-master-slave/src/main/java/com/shardingjdbc/service/impl/UserServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * @author yangbin
+ */
 @Service
 public class UserServiceImpl implements UserService {
 
